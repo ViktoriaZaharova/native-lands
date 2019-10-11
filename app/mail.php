0c91ab6411b245b40045000a554f7fa360d99f6f
ViktoriaZaharova <zaharova181114@gmail.com>
2019-10-11 12:38:42 +0300

    Initial commit

---
+++ app/mail.php
@@ -0,0 +1,10 @@
+<?php
+$recepient = "info@gmail.com"; //заменить на свой почтовый ящик
+$sitename = "Родные земли";
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$mail = trim($_POST["mail"]);
+$message = trim($_POST["message"]);
+$message = "Имя: $name \nТелефон: $phone \nEmail: $mail \nСообщение: $message";
+$pagetitle = "Заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
